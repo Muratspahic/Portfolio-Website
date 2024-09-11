e01b488bf5b9d03674a902066a54280434efe126
Muratspahic <emir96.prf@gmail.com>
2024-09-11 16:52:09 +0200

    add: tests for portfolio section and contact section

--- cypress/e2e/portfolio.cy.js
+++ cypress/e2e/portfolio.cy.js
@@ -6,58 +6,48 @@ describe('Visit My Portfolio', () => {
   });
 
   it('should contain the header element', () => {
-    cy.get('#header').should('be.visible');
-    cy.get('#header .logo').should('be.visible');
+    cy.get('#header').within(() => {
+      cy.get('.logo').should('be.visible');
 
-    cy.get('#header #sidemenu')
-    .should('be.visible')
-    .within(() => {
-        cy.get('a[href="#home"]').should('be.visible')
-        cy.get('a[href="#about"]').should('be.visible')
-        cy.get('a[href="#services"]').should('be.visible')
-        cy.get('a[href="#portfolio"]').should('be.visible')
-        cy.get('a[href="#contact"]').should('be.visible')
-    });
+      cy.get('#sidemenu').within(() => {
+        cy.get('a[href="#home"]').should('be.visible');
+        cy.get('a[href="#about"]').should('be.visible');
+        cy.get('a[href="#services"]').should('be.visible');
+        cy.get('a[href="#portfolio"]').should('be.visible');
+        cy.get('a[href="#contact"]').should('be.visible');
+      });
 
-    cy.get('.header-text')
-    .should('be.visible')
-    .within(() => {
+      cy.get('.header-text').within(() => {
         cy.get('span').should('be.visible').and('have.length', 1);
         cy.get('p').should('be.visible').and('have.length', 1);
         cy.get('h1').should('be.visible');
+      });
     });
   });
 
+
   it('should contain the about section', () => {
-    cy.get('#about').should('be.visible');
+    cy.get('#about').within(() => {
+      cy.get('.about-col-1').within(() => {
+        cy.get('img').should('be.visible');
+      });
 
-    cy.get('#about .about-col-1')
-      .within(() => {
-      cy.get('img').should('be.visible');
-    });
+      cy.get('h1').should('exist');
+      cy.get('p').first().should('exist');
 
-    cy.get('#about')
-    .should('be.visible')
-    .within(() => {
-        cy.get('h1').should('exist');
-        cy.get('p').first().should('exist');
-    });
-
-    cy.get('[data-testid="links"]')
-    .should('be.visible')
-    .within(() => {
-      cy.get('.tab-links').should('have.length', tabTexts.length);
-      cy.get('.tab-links').eq(0).should('have.class', 'active-link');
+      cy.get('[data-testid="links"]').within(() => {
+        cy.get('.tab-links').should('have.length', tabTexts.length);
+        cy.get('.tab-links').eq(0).should('have.class', 'active-link');
+      });
     });
   });
 
   it('should correctly display content when tabs are clicked', () => {
     const testTab = (index, expectedContentId) => {
-        cy.get('[data-testid="links"] .tab-links').eq(index).should('be.visible');
-        cy.get('[data-testid="links"] .tab-links').eq(index).click();
+      cy.get('[data-testid="links"] .tab-links').eq(index).click();
 
-        cy.get(expectedContentId).should('be.visible');
-        cy.get('.tab-contents').not(expectedContentId).should('not.be.visible');
+      cy.get(expectedContentId).should('be.visible');
+      cy.get('.tab-contents').not(expectedContentId).should('not.be.visible');
     };
 
     testTab(0, '#skills');
@@ -66,39 +56,45 @@ describe('Visit My Portfolio', () => {
   });
 
   it('should render the services section and display all services with correct details', () => {
-    cy.get('[data-testid="services"]').should('exist');
-    cy.get('[data-testid="services"] > div').should('have.length', 3);
+    cy.get('[data-testid="services"]').within(() => {
+      cy.get('> div').should('have.length', 3);
 
-    cy.get('[data-testid="services"] > div').eq(0).within(() => {
+      cy.get('> div').eq(0).within(() => {
         cy.get('h2').should('be.visible');
         cy.get('p').should('be.visible');
         cy.get('a').should('be.visible');
-    });
+      });
 
-    cy.get('[data-testid="services"] > div').eq(1).within(() => {
+      cy.get('> div').eq(1).within(() => {
         cy.get('h2').should('be.visible');
         cy.get('p').should('be.visible');
         cy.get('a').should('be.visible');
-    });
+      });
 
-    cy.get('[data-testid="services"] > div').eq(2).within(() => {
+      cy.get('> div').eq(2).within(() => {
         cy.get('h2').should('be.visible');
         cy.get('p').should('be.visible');
         cy.get('a').should('be.visible');
+      });
     });
   });
+
 
   it('should display the portfolio section with the correct ID', () => {
     cy.get('#portfolio').should('be.visible');
   });
 
   it('should display the correct title in the portfolio section', () => {
-    cy.get('#portfolio h1').should('be.visible');
+    cy.get('#portfolio').within(() => {
+      cy.get('h1').should('be.visible');
+    });
   });
 
   it('should display all work items', () => {
-    cy.get('#portfolio .work-list').within(() => {
-      cy.get('.work').should('have.length', 3);
+    cy.get('#portfolio').within(() => {
+      cy.get('.work-list').within(() => {
+        cy.get('.work').should('have.length', 3);
+      });
     });
   });
 
@@ -112,12 +108,56 @@ describe('Visit My Portfolio', () => {
 
   it('should verify the "See more" button existence, visibility, text, and clickability', () => {
     cy.get('[data-testid="btn-more"]')
-      .should('exist')
-      .should('be.visible')
-      .should('have.text', 'See more')
-      .click()
-      cy.window().then((win) => {
-        cy.wrap(win.scrollY).should('eq', 0);
+    .should('exist')
+    .should('be.visible')
+    .should('have.text', 'See more')
+    .click()
+    cy.window().then((win) => {
+      cy.wrap(win.scrollY).should('eq', 0);
+    });
+  });
+
+  it('should display the contact section', () => {
+    cy.get('[data-testid="contact-info"]').should('be.visible');
+  });
+
+  it('should display the contact information correctly', () => {
+    cy.get('[data-testid="contact-info"]').within(() => {
+      cy.get('.sub-title').should('be.visible');
+      cy.get('p').first().should('be.visible');
+      cy.get('p').eq(1).should('be.visible');
+    });
+  });
+
+  it('should have visible social media links', () => {
+    cy.get('[data-testid="contact-info"]').within(() => {
+      cy.get('.social-icons a').should('have.length.greaterThan', 0).each(($el) => {
+        cy.wrap($el).should('be.visible');
       });
+    });
+  });
+
+  it('should display the download CV button and be clickable', () => {
+    cy.get('[data-testid="btn-info"]').should('be.visible')
+      .and('have.attr', 'href', './Images/my-cv.pdf')
+      .and('have.attr', 'download');
+  });
+
+  it('should be visible and functional', () => {
+    cy.get('[data-testid="login-info"]').within(() => {
+      cy.get('form').should('be.visible');
+
+      cy.get('input[name="Name"]').type('Emir Pepi').should('have.value', 'Emir Pepi');
+      cy.get('input[name="email"]').type('emir.pepi@example.com').should('have.value', 'emir.pepi@example.com');
+      cy.get('textarea[name="Message"]').type('This is a test message.').should('have.value', 'This is a test message.');
+
+      cy.get('button[data-testid="btn-submit"]').should('be.visible').click();
+    });
+  });
+
+  it('should display the copyright information with correct text', () => {
+    cy.get('[data-testid="copyright-info"]').within(() => {
+      cy.get('p').should('be.visible').and('have.text', 'Copyright © Emir 2024 Portfolio Page');
+    });
   });
 });
